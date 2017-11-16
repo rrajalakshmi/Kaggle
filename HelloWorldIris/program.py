0538cbaa4f8f5679c16096346015193075ed5034
rrajalakshmi <ramesh.rajalakshmi@gmail.com>
2017-11-16 01:05:26 -0600

    Metrics for KNN and LR on Iris dataset

--- HelloWorldIris/program.py
+++ HelloWorldIris/program.py
@@ -37,3 +37,51 @@ plt._show()
 # scatter plot matrix
 scatter_matrix(dataset)
 plt.show()
+
+# Split data set with 80% training and 20% validation
+array = dataset.values
+X = array[:, 0:4]
+# Get only the last label as output
+y = array[:, 4]
+
+validation_size = 0.2
+seed = 7
+X_train, X_validation, y_train, y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
+
+scoring = 'accuracy'
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+results = []
+names = []
+
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
+# Make predictions on validation dataset
+print('KNeighbors')
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(y_validation, predictions))
+print(confusion_matrix(y_validation, predictions))
+print(classification_report(y_validation, predictions))
+
+print('Logistic Regression')
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+predictions = lr.predict(X_validation)
+print(accuracy_score(y_validation, predictions))
+print(confusion_matrix(y_validation, predictions))
+print(classification_report(y_validation, predictions))
